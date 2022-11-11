83903deee779bf88d53b73263fd7d18f6a91d747
irenesyr <yis189@ucsd.edu>
2022-11-10 21:52:07 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import {BakeryItem} from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,34 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [counter, setCounter] = useState(0);
+  const [food, setFood] = useState([]);
+
+  function handleClick(){
+    setCounter(counter+1);
+  }
+
+  function handleCount(name, val){
+    setCounter(counter + val);
+    if (!food.includes(name)){
+      setFood(old => [...old, name]);
+    }
+  }
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
+
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem key={index} item={item} addCounter={handleCount}/> // replace with BakeryItem component
       ))}
 
       <div>
+      <h2>Total Price: {counter}</h2>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {food.map((item) => 
+        <li>{item}</li>)}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,20 @@
 // TODO: create a component that displays a single bakery item
+// import bakeryData from "./data.js";
+
+export function BakeryItem({item, addCounter}) {
+    return (
+        <div>
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+            <img 
+            src={item.image}
+            />
+            <p>
+                {item.description}
+            </p>
+            <button onClick={() => addCounter(item.name, item.price)}>Click me!</button>
+            </div>
+
+    );
+  }
+  
